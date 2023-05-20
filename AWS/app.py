36cc30329c7180a553ed86a8ad1e382bfed0de25
Ignacio Garcia Barrero <ignaciogarciabarrero@Ignacios-MBP.home>
2023-05-20 20:39:16 +0200

    managed endpoint

--- AWS/app.py
+++ AWS/app.py
@@ -1,22 +1,22 @@
-from flask import Flask, render_template, request,jsonify
+from flask import Flask, request, jsonify
 import pickle
 import numpy as np
 
 model = pickle.load(open('kmeans_model.pkl', 'rb'))
 
-
 app = Flask(__name__)
-
 
 
 @app.route('/')
 def man():
     # return render_template('home.html')
-    return "<h2>THIS IS OUR K-MEANS HOME PAGE</h2>"
+    return "<h2>THIS IS OUR K-MEANS HOME PAGE AND WE ARE ABOUT TO TEST OUR MODEL</h2>"
 
+# Tipo 0 (saver)
+# /api/k-means/prediction?data=2&data=1&data=0&data=0&data=0&data=0&data=0&data=1&data=0&data=0&data=1&data=1&data=0&data=0&data=1&data=1&data=0
 
-# api/v2/resources/muchosbooks?id=1&id=2
-# data=2&data=1&data=0&data=0&data=0&data=0&data=0&data=1&data=0&data=0&data=1&data=1&data=0&data=0&data=1&data=1&data=0
+# Tipo 1 (spender)
+# /api/k-means/prediction?data=0&data=0&data=0&data=1&data=0&data=1&data=1&data=1&data=1&data=1&data=0&data=1&data=0&data=1&data=0&data=0&data=1
 @app.route('/api/k-means/prediction', methods=['GET'])
 def api_data():
 
@@ -27,26 +27,20 @@ def api_data():
             if combinacion[0]=="data":
                 mis_datas = combinacion[1]
                 for mi_data in mis_datas:
-                    results.append(int(mi_data))
-                    arr = np.array(results)
-                    pred = model.predict(arr)
-                return jsonify(pred)
+                    try:
+                        results.append(int(mi_data))
+                    except:
+                        return jsonify("invalid format")
 
-#     arr = np.array([[data1, data2, data3, data4]])
-#     pred = model.predict(arr)
-
-
-
-# @app.route('/predict', methods=['POST'])
-# def home(): 
-#     data1 = request.form['a']
-#     data2 = request.form['b']
-#     data3 = request.form['c']
-#     data4 = request.form['d']
-#     arr = np.array([[data1, data2, data3, data4]])
-#     pred = model.predict(arr)
-#     return render_template('after.html', data=pred)
-
+                arr = np.array([results]).reshape(1, -1)
+                pred = model.predict(arr)
+                if pred[0]==0:
+                    value ="saver"
+                elif pred[0]==1:
+                    value="spender"
+                else:
+                    value="unknown"
+                return jsonify(value)
 
 if __name__ == "__main__":
     app.run(debug=True)
